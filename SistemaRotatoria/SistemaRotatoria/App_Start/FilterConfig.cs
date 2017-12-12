5338fc5767a331cb9bf53a025f7fbb7df3d40e05
User <User@USER-PC>
2017-12-12 11:48:27 -0200

    Criação do Projeto

---
+++ SistemaRotatoria/SistemaRotatoria/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaRotatoria
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SistemaRotatoria/SistemaRotatoria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaRotatoria.MvcApplication" Language="C#" %>
